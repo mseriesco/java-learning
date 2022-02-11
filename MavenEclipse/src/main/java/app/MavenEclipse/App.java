46dd99c0e9e9f5028e9e9bf5019f5d5743428db6
Mohsin Nazir <mseriesco@gmail.com>
2022-02-12 01:19:13 +0500

    Maven Projects Created

---
+++ MavenEclipse/src/main/java/app/MavenEclipse/App.java
@@ -0,0 +1,13 @@
+package app.MavenEclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World Maven and Eclipse!" );
+    }
+}
